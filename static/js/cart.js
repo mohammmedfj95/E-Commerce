1480e974b84d7badf6fd434b3696a0fca64712e4
mohammmedfj95 <167021147+mohammmedfj95@users.noreply.github.com>
2024-04-21 17:51:49 +0300

    2.BackEnd

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,11 +1,39 @@
+var updateBtns = document.getElementsByClassName('update-cart')
 
-console.log('Hello World')
-var updateBtns = document.getElementsByName('update-cart')
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
 
-for (i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function (){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('Product Id:', productId, ' | Action: ', action)
-    })
+		if (user == 'AnonymousUser'){
+			console.log('User is not authenticated')
+			
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
+
